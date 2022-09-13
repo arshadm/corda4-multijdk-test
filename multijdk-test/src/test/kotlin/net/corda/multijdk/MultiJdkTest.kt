300529dc7cb796513312d22ef6a2c15cef59e9e5
Arshad Mahmood <1391251+arshadm@users.noreply.github.com>
2022-09-13 14:21:56 +0100

    wip jdk11 tests

--- multijdk-test/src/test/kotlin/net/corda/multijdk/MultiJdkTest.kt
+++ multijdk-test/src/test/kotlin/net/corda/multijdk/MultiJdkTest.kt
@@ -18,11 +18,12 @@ import net.corda.testing.node.internal.findCordapp
 import org.junit.Test
 import java.io.File
 
-class Jdk8NodesTest {
+class MultiJdkTest {
 
     @Test(timeout = 300_000)
     fun `Two nodes both running JDK 8, with cordapps also targeting JDK 8`() {
         val jdk8ClassPath = generateClassPath("jdk8")
+        val java8Home = System.getProperty("jdk8_home")
         val cordapps = listOf(findCordapp("net.corda.cordapp.java.jdk8.helloworld.flows"))
 
         val user = User("mark", "dadada", setOf(Permissions.all()))
@@ -30,10 +31,44 @@ class Jdk8NodesTest {
             DriverParameters(
                 isDebug = false, startNodesInProcess = false,
                 notarySpecs = listOf(NotarySpec(name = DUMMY_NOTARY_NAME, startInProcess = false)),
-                notaryCustomOverrides = mapOf("classPath" to jdk8ClassPath)
+                notaryCustomOverrides = mapOf("javaHome" to java8Home, "classPath" to jdk8ClassPath)
             )
         ) {
-            val defaultNodeParameters = NodeParameters(rpcUsers = listOf(user), classPath = jdk8ClassPath, additionalCordapps = cordapps)
+            val defaultNodeParameters = NodeParameters(
+                rpcUsers = listOf(user),
+                javaHome = java8Home,
+                classPath = jdk8ClassPath,
+                additionalCordapps = cordapps
+            )
+
+            val nodeAHandle = startNode(providedName = ALICE_NAME, defaultParameters = defaultNodeParameters).getOrThrow()
+            val nodeBHandle = startNode(providedName = BOB_NAME, defaultParameters = defaultNodeParameters).getOrThrow()
+            nodeAHandle.rpc.let {
+                val ref = it.startFlow(::HelloWorldFlowInitiator, nodeBHandle.nodeInfo.singleIdentity()).returnValue.getOrThrow(20.seconds)
+            }
+        }
+    }
+
+    @Test(timeout = 300_000)
+    fun `Two nodes both running JDK 11, with cordapps also targeting JDK 11`() {
+        val jdk8ClassPath = generateClassPath("jdk11")
+        val java11Home = System.getProperty("jdk11_home")
+        val cordapps = listOf(findCordapp("net.corda.cordapp.java.jdk11.helloworld.flows"))
+
+        val user = User("mark", "dadada", setOf(Permissions.all()))
+        driver(
+            DriverParameters(
+                isDebug = false, startNodesInProcess = false,
+                notarySpecs = listOf(NotarySpec(name = DUMMY_NOTARY_NAME, startInProcess = false)),
+                notaryCustomOverrides = mapOf("javaHome" to java11Home, "classPath" to jdk8ClassPath)
+            )
+        ) {
+            val defaultNodeParameters = NodeParameters(
+                rpcUsers = listOf(user),
+                javaHome = java11Home,
+                classPath = jdk8ClassPath,
+                additionalCordapps = cordapps
+            )
 
             val nodeAHandle = startNode(providedName = ALICE_NAME, defaultParameters = defaultNodeParameters).getOrThrow()
             val nodeBHandle = startNode(providedName = BOB_NAME, defaultParameters = defaultNodeParameters).getOrThrow()
