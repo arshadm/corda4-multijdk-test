ec35a3c647f363c758144d7c83b7e1e24e60a484
Arshad Mahmood <1391251+arshadm@users.noreply.github.com>
2022-09-20 14:57:17 +0100

    added tests for nodes running different versions of java

--- multijdk-test/src/test/kotlin/net/corda/multijdk/HelloWorldTest.kt
+++ multijdk-test/src/test/kotlin/net/corda/multijdk/HelloWorldTest.kt
@@ -19,7 +19,7 @@ import net.corda.testing.node.internal.findCordapp
 import org.junit.Test
 import java.io.File
 
-class MultiJdkTest {
+class HelloWorldTest {
 
     @Test(timeout = 300_000)
     fun `Two nodes both running JDK 8, with cordapps also targeting JDK 8`() {
@@ -75,6 +75,74 @@ class MultiJdkTest {
             val nodeBHandle = startNode(providedName = BOB_NAME, defaultParameters = defaultNodeParameters).getOrThrow()
             nodeAHandle.rpc.let {
                 val ref = it.startFlow(::Jdk11HelloWorldFlowInitiator, nodeBHandle.nodeInfo.singleIdentity()).returnValue.getOrThrow(20.seconds)
+            }
+        }
+    }
+
+    @Test(timeout = 300_000)
+    fun `Cordapp targeting JDK 8, initiated by node running JDK8 and other party running on JDK11`() {
+        val java8Home = System.getProperty("jdk8_home")
+        val java11Home = System.getProperty("jdk11_home")
+        val jdk8ClassPath = generateClassPath("jdk8")
+        val jdk11ClassPath = generateClassPath("jdk11")
+        val cordapps = listOf(findCordapp("net.corda.cordapp.java.jdk8.helloworld.flows"))
+
+        val user = User("mark", "dadada", setOf(Permissions.all()))
+        driver(
+            DriverParameters(
+                isDebug = false, startNodesInProcess = false,
+                notarySpecs = listOf(NotarySpec(name = DUMMY_NOTARY_NAME, startInProcess = false)),
+                notaryCustomOverrides = mapOf("javaHome" to java8Home, "classPath" to jdk8ClassPath)
+            )
+        ) {
+            val defaultNodeParameters = NodeParameters(
+                rpcUsers = listOf(user),
+                additionalCordapps = cordapps
+            )
+
+            val nodeAHandle = startNode(providedName = ALICE_NAME, defaultParameters = defaultNodeParameters,
+                javaHome = java8Home, classPath = jdk8ClassPath).getOrThrow()
+            val nodeBHandle = startNode(providedName = BOB_NAME, defaultParameters = defaultNodeParameters,
+                javaHome = java11Home, classPath = jdk11ClassPath).getOrThrow()
+            nodeAHandle.rpc.let {
+                val ref =
+                    it.startFlow(::Jdk8HelloWorldFlowInitiator, nodeBHandle.nodeInfo.singleIdentity()).returnValue.getOrThrow(20.seconds)
+            }
+        }
+    }
+
+    @Test(timeout = 300_000)
+    fun `Cordapp targeting JDK 8, initiated by node running JDK11 and other party running on JDK8`() {
+        val java8Home = System.getProperty("jdk8_home")
+        val java11Home = System.getProperty("jdk11_home")
+        val jdk8ClassPath = generateClassPath("jdk8")
+        val jdk11ClassPath = generateClassPath("jdk11")
+        val cordapps = listOf(findCordapp("net.corda.cordapp.java.jdk8.helloworld.flows"))
+
+        val user = User("mark", "dadada", setOf(Permissions.all()))
+        driver(
+            DriverParameters(
+                isDebug = false, startNodesInProcess = false,
+                notarySpecs = listOf(NotarySpec(name = DUMMY_NOTARY_NAME, startInProcess = false)),
+                notaryCustomOverrides = mapOf("javaHome" to java8Home, "classPath" to jdk8ClassPath)
+            )
+        ) {
+            val defaultNodeParameters = NodeParameters(
+                rpcUsers = listOf(user),
+                additionalCordapps = cordapps
+            )
+
+            val nodeAHandle = startNode(
+                providedName = ALICE_NAME, defaultParameters = defaultNodeParameters,
+                javaHome = java11Home, classPath = jdk11ClassPath
+            ).getOrThrow()
+            val nodeBHandle = startNode(
+                providedName = BOB_NAME, defaultParameters = defaultNodeParameters,
+                javaHome = java8Home, classPath = jdk8ClassPath
+            ).getOrThrow()
+            nodeAHandle.rpc.let {
+                val ref =
+                    it.startFlow(::Jdk8HelloWorldFlowInitiator, nodeBHandle.nodeInfo.singleIdentity()).returnValue.getOrThrow(20.seconds)
             }
         }
     }
